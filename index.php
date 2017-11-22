0950c7f54bddef57f29a66d2a7d5557c5585182e
Daniel <apps.DanielLarsson@gmail.com>
2017-11-22 20:42:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Barlow" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    
+    <title>Document</title>
+</head>
+<body>
+<?php include('navbar.php')?>
+
+
+
+</form>
+<h1 align="center" style="background-color: green;">Hej Hej!</h1>
+<?php
+/*
+require('db.php');
+
+
+$sql = "SELECT customer_id, customer_firstname, customer_surname FROM customer";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["customer_id"]. " - Name: " . $row["customer_firstname"]. " " . $row["customer_surname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+
+*/
+
+?>  
+</body>
+</html>
+
