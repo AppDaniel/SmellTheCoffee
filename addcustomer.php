af85a2f758a274c5edabd0c86286c95a1fc2ca48
Daniel <apps.DanielLarsson@gmail.com>
2017-11-27 10:41:32 +0100

    Update addcustomer.php

--- addcustomer.php
+++ addcustomer.php
@@ -1,21 +1,23 @@
 <?php
+session_start();
 header("Location: index.php");
 require('db.php');
 
+
 $customer_firstname = $_POST['customer_firstname'];
-$customer_surname = $_POST['customer_surname'];
+$customer_lastname = $_POST['customer_lastname'];
 $customer_email = $_POST['customer_email'];
 $customer_password = $_POST['customer_password'];
 $customer_address = $_POST['customer_address'];
 $customer_zip = $_POST['customer_zip'];
 $customer_city = $_POST['customer_city'];
 
+$customer_password_hash = password_hash($customer_password, PASSWORD_BCRYPT);
 
 
-
-$sql = "INSERT INTO customer (customer_firstname, customer_surname, customer_email, 
+$sql = "INSERT INTO customer (customer_firstname, customer_lastname, customer_email, 
 customer_password, customer_address, customer_zip, customer_city) 
-VALUES ('$customer_firstname', '$customer_surname', '$customer_email', 
-'$customer_password', '$customer_address', '$customer_zip', '$customer_city')"; 
+VALUES ('$customer_firstname', '$customer_lastname', '$customer_email', 
+'$customer_password_hash', '$customer_address', '$customer_zip', '$customer_city')"; 
 mysqli_query($conn, $sql) or die (mysqli_error($conn));
 ?>
