dd0f48bb133446def7ee6f38d7151f360f3792af
Daniel <apps.DanielLarsson@gmail.com>
2017-11-27 10:49:30 +0100

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -1,9 +1,13 @@
 <?php
+session_start(); 
 require('db.php');
 
-// Hämtar fårn idex formuläret
+
+
+// Hämtar fårn index formuläret
 $user = $_POST['user'];
 $pass = $_POST['pass'];
+
 
 //Hämtar users
 $sql = "SELECT * FROM customer WHERE customer_email = '$user'";
@@ -13,14 +17,29 @@ $query = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 
 $row = mysqli_fetch_array($query);
 //Jämför password med user
-$dbpassword = $row['customer_password']; 
+$hash = $row['customer_password'];
 
-if($pass == $dbpassword){
-    echo "Du är nu inne i systemet";
+if (password_verify($pass, $hash)) {
+    $_SESSION['customer_id']=$row['customer_id'];
+    $_SESSION['customer_firstname']=$row['customer_firstname'];
+    $_SESSION['customer_lastname']=$row['customer_lastname'];
+
+    $_SESSION['customer_email']=$row['customer_email'];
+    $_SESSION['customer_password']=$row['customer_password'];
+    $_SESSION['customer_address']=$row['customer_address'];
+    $_SESSION['customer_zip']=$row['customer_zip'];
+    $_SESSION['customer_city']=$row['customer_city'];
+
+   
+    
+    header("Location: index.php");
 
 } else{
 
+    $_SESSION['error'] = '<h1 style="color: red;">Fel anv eller email</h1>';
     header("Location: login.php");
-}
+    
+    } 
 
+if(isset($_POST['submit'])) { SignIn(); } 
 ?>
